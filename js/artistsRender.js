42739f214364dbc1d488b50325f788e79a555a9e
unknown <jchernandez827@gmail.com>
2021-06-30 17:33:27 -0500

    Add Sponsors And Footer Sections For Home

--- js/artistsRender.js
+++ js/artistsRender.js
@@ -4,45 +4,45 @@
 const artistContainer = document.querySelector('.artists-container');
 const cardsInfo = [
   {
-    imgUrl: '../assets/img/banbBack.jpeg',
-    title: 'The Blusy Tusy',
-    genres: 'Blues/Jazz/Soul',
+    imgUrl: '../assets/img/bandBack.jpeg',
+    title: 'The Blusy Brothers',
+    genres: 'R&B / Fusion / Rock',
     text: 'King of the Delta Blues Singers, The Complete Recordings, King of the Delta Blues.',
   },
   {
-    imgUrl: '../assets/img/banbBack.jpeg',
-    title: 'The Blusy Tusy',
-    genres: 'Blues/Jazz/Soul',
+    imgUrl: '../assets/img/bandBack2.jpeg',
+    title: 'Carl Weathersby',
+    genres: 'Dance / Blues / Soul',
     text: 'King of the Delta Blues Singers, The Complete Recordings, King of the Delta Blues.',
   },
   {
-    imgUrl: '../assets/img/banbBack.jpeg',
-    title: 'The Blusy Tusy',
-    genres: 'Blues/Jazz/Soul',
+    imgUrl: '../assets/img/bandBack3.jpeg',
+    title: 'Sherman Robertson',
+    genres: 'Country / Blues / SoulJazz',
     text: 'King of the Delta Blues Singers, The Complete Recordings, King of the Delta Blues.',
   },
   {
-    imgUrl: '../assets/img/banbBack.jpeg',
-    title: 'The Blusy Tusy',
-    genres: 'Blues/Jazz/Soul',
+    imgUrl: '../assets/img/bandBack4.jpeg',
+    title: 'Lonnie Shields',
+    genres: 'Blues / Jazz / Soul',
     text: 'King of the Delta Blues Singers, The Complete Recordings, King of the Delta Blues.',
   },
   {
-    imgUrl: '../assets/img/banbBack.jpeg',
-    title: 'The Blusy Tusy',
-    genres: 'Blues/Jazz/Soul',
+    imgUrl: '../assets/img/bandBack5.jpeg',
+    title: "Lil' Dave Thompson",
+    genres: 'Rock / Jazz / Soul',
     text: 'King of the Delta Blues Singers, The Complete Recordings, King of the Delta Blues.',
   },
   {
-    imgUrl: '../assets/img/banbBack.jpeg',
-    title: 'The Blusy Tusy',
-    genres: 'Blues/Jazz/Soul',
+    imgUrl: '../assets/img/bandBack6.jpeg',
+    title: 'Valerie Wellington',
+    genres: 'Bluegrass / R&B / Soul',
     text: 'King of the Delta Blues Singers, The Complete Recordings, King of the Delta Blues.',
   },
 ];
 
 function artistGenerator() {
-  for (let i = 0; i < cardsInfo.length; i++) {
+  for (let i = 0; i <= cardsInfo.length; i++) {
     const card = document.createElement('ul');
     card.classList.add('card');
     const imageContainer = document.createElement('li');
@@ -50,7 +50,7 @@ function artistGenerator() {
     card.appendChild(imageContainer);
     const cardImg = document.createElement('div');
     cardImg.classList.add('card-img');
-    cardImg.style.backgoundImage = cardsInfo[i].imgUrl;
+    cardImg.style.backgroundImage = `url(${cardsInfo[i].imgUrl})`;
     imageContainer.appendChild(cardImg);
     const cardText = document.createElement('li');
     cardText.classList.add('card-text');
